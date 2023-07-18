3774505d2b6ff42772ec022244881c0dc2ec24ee
Meng Lin <menglinmaker@gmail.com>
2023-07-18 21:03:01 +1000

    Reduce length of functions

--- src/load.js
+++ src/load.js
@@ -1,4 +1,4 @@
-var script2, gbloburl
+var script2
 
 function handleFileSelect(files) {
   if (!files || files.length == 0 || !files[0]) return
@@ -17,38 +17,26 @@ function handleFileSelect(files) {
 function init2() {
   var fileloader = _getid("fileload1")
   fileloader.setAttribute("accept", ".swf")
-  fileloader.onchange = e => {
-    if (!e || !e.target) {
-      alert("Cannot load file.")
-      return
-    }
-    handleFileSelect(e.target.files)
-  }
+  fileloader.onchange = e => handleFileSelect(e.target.files)
 }
 
-function proc_loadscript(scriptSrc, fileUrl) {
-  fetch(scriptSrc)
-  .then(async(response) => {
-    script2 = await response.text()
-    proc_loadgame(fileUrl)
-  })
-  .catch(e => alert("Error. Cannot load emulator scripts."))
+async function proc_loadscript(scriptSrc) {
+  const response = await fetch(scriptSrc)
+  if (!response) alert("Error. Cannot load emulator scripts.")
+  script2 = await response.text()
 }
 
-function proc_loadgame(url) {
+async function proc_loadgame(url) {
   if (!window.WebAssembly) {
     alert('Flash Emulator not supported. Please upgrade your browser.')
     return
   }
 
-  if (!script2) {
-    proc_loadscript("emulator/emulator.php", url)
-    return
-  }
+  if (!script2) await proc_loadscript("emulator/emulator.php")
 
   gbloburl = url
 
-  var emulator = _getid("emulator2")
+  emulator = _getid("emulator2")
   var emulatorIframe = _getfrmdoc(emulator) 
   
   emulator.onload = function () {
